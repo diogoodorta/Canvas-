c68d5a90d8a3c19dce83b5b4a54159ecd62f5bde
Diogo Silva Dorta <101647390+diogoodorta@users.noreply.github.com>
2022-03-21 11:31:49 -0300

    adição da segunda parte do projeto

--- script.js
+++ script.js
@@ -4,42 +4,72 @@ var ctx=c.getContext("2d");
 ctx.fillStyle="black";
 ctx.fillRect(-50,500,500,500);
 
-ctx.moveTo(1,500);
-ctx.lineTo(250,50);
+ctx.strokeStyle="red";
+ctx.fillStyle="red";
+ctx.beginPath();
+ctx.arc(850, 150, 150, 1, Math.PI/3, true);
+ctx.stroke();
+ctx.fill();
+
+ctx.strokeStyle="#8b4513";
+ctx.fillStyle="#8b4513";
+ctx.beginPath();
+ctx.moveTo(0,300);
+ctx.lineTo(150,50);
 ctx.lineTo(390,300);
-ctx.strokeStyle="black";
 ctx.stroke();
+ctx.fill();
 
-ctx.moveTo(300,500);
+
+ctx.strokeStyle="#8b4513";
+ctx.fillStyle="#8b4513";
+ctx.beginPath();
+ctx.moveTo(200,500);
 ctx.lineTo(500,50);
-ctx.lineTo(700,500);
-ctx.strokeStyle="black";
+ctx.lineTo(800,500);
 ctx.stroke();
+ctx.fill();
 
+ctx.strokeStyle="#8b4513";
+ctx.fillStyle="#8b4513";
+ctx.beginPath();
 ctx.moveTo(610,300);
 ctx.lineTo(770,50);
 ctx.lineTo(940,300);
-ctx.strokeStyle="black";
 ctx.stroke();
+ctx.fill();
 
+
+ctx.strokeStyle="#8b4513";
+ctx.fillStyle="#8b4513";
+ctx.beginPath();
 ctx.moveTo(850,500);
 ctx.lineTo(1050,50);
 ctx.lineTo(1300,500);
-ctx.strokeStyle="black";
 ctx.stroke();
+ctx.fill();
 
+ctx.strokeStyle="#8b4513";
+ctx.fillStyle="#8b4513";
+ctx.beginPath();
 ctx.moveTo(1190,300);
 ctx.lineTo(1350,50);
 ctx.lineTo(1540,300);
-ctx.strokeStyle="black";
 ctx.stroke();
+ctx.fill();
 
-ctx.moveTo(1450,500);
-ctx.lineTo(1650,50);
-ctx.lineTo(1900,500);
-ctx.strokeStyle="black";
+
+ctx.strokeStyle="#8b4513";
+ctx.fillStyle="#8b4513";
+ctx.beginPath();
+ctx.moveTo(1380,500);
+ctx.lineTo(1750,50);
+ctx.lineTo(2100,500);
 ctx.stroke();
+ctx.fill();
 
+ctx.fillStyle="#8b4513";
+ctx.fillRect(-2,300,2000,200);
 
 ctx.fillStyle="black";
 ctx.fillRect(200,500,1500,200);
@@ -47,8 +77,10 @@ ctx.fillRect(200,500,1500,200);
 ctx.fillStyle="black";
 ctx.fillRect(1500,500,500,500);
 
-ctx.fillStyle="brown";
+ctx.fillStyle="#8b4513";
 ctx.fillRect(400,700,1100,300);
+
+  
 
 
 
