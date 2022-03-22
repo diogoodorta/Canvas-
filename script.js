0092a4b20f430b1064212c6fb0026ebd29ed61eb
Diogo Silva Dorta <101647390+diogoodorta@users.noreply.github.com>
2022-03-22 12:10:35 -0300

    terceira adição do projeto

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-
 var c=document.getElementById("canvas"); 
 var ctx=c.getContext("2d");
+var img=new Image; 
+
 ctx.fillStyle="black";
 ctx.fillRect(-50,500,500,500);
 
-ctx.strokeStyle="red";
-ctx.fillStyle="red";
-ctx.beginPath();
-ctx.arc(850, 150, 150, 1, Math.PI/3, true);
-ctx.stroke();
-ctx.fill();
 
 ctx.strokeStyle="#8b4513";
 ctx.fillStyle="#8b4513";
@@ -27,15 +22,6 @@ ctx.beginPath();
 ctx.moveTo(200,500);
 ctx.lineTo(500,50);
 ctx.lineTo(800,500);
-ctx.stroke();
-ctx.fill();
-
-ctx.strokeStyle="#8b4513";
-ctx.fillStyle="#8b4513";
-ctx.beginPath();
-ctx.moveTo(610,300);
-ctx.lineTo(770,50);
-ctx.lineTo(940,300);
 ctx.stroke();
 ctx.fill();
 
@@ -80,13 +66,17 @@ ctx.fillRect(1500,500,500,500);
 ctx.fillStyle="#8b4513";
 ctx.fillRect(400,700,1100,300);
 
-  
+ctx.strokeStyle="red";
+ctx.fillStyle="red";
+ctx.beginPath();
+ctx.shadowBlur = 30;
+ctx.shadowColor = "yellow";
+ctx.arc(800, 140, 150, 1, Math.PI/3, true);
+ctx.stroke(); 
+ctx.fill();
 
-
-
-
-
-
-
-
+img.onload=function(){
+	ctx.drawImage(img, 50, 200);
+};
+img.src= 'img/carro.png';
 
